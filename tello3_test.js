df6ab85f6c0388c1e5b6e0ed224295d2f1d5d2a5
tom <f41ardu@aror.de>
2019-06-30 15:33:26 +0200

    Add udp listener

--- tello3_test.js
+++ tello3_test.js
@@ -13,6 +13,66 @@
  */
 
 (function(ext) {
+// Tello udp port and IP address
+   var PORT = 8889 ;
+   var HOST = '192.168.10.1'; // Tello IP
+   // var HOST = '127.0.0.1'; // Test localhost (debug mode)
+   
+   // Scratch listener port 
+   var listenerPort = 8890;  
+   var listenerHOST = '0.0.0.0';
+
+   // udp connector  
+   var dgram = require('dgram');
+   // client connector (send commands) 
+   var client = dgram.createSocket('udp4');
+   // server connector (receive Telle response) 
+   var server1 = dgram.createSocket('udp4');
+   
+   
+   // initial variables
+   var myStatus = 1; // initially set status to yellow
+   var connected = false; // initially set connected to false
+   var getData = ' '; // initial set blank 
+   var dict = {
+	'speed?' : 3, // 4, 5 vector length
+	'battery?' : 10, // % percentage
+	'time?' : 12, // Motors on time
+	'height?' : 9, 
+	'temp?' : 6, // 7 average
+	'attitude?' : 27,
+	'baro?' : 11, // baramoter measurement cm
+	'acceleration?' : 0, // 1, 2 vector length
+	'tof?' : 8, // distance cm
+   }; 
+   
+   // Scratch UDP Listener (experimental) 
+   ext.cnct = function() {	
+   if (connected == false) {
+		server1.on("error", function (err) {
+			alert("server error:\n" + err.stack);
+			server.close();
+		});
+
+		server1.on("message", function (msg, rinfo) {
+			//setReceived("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port); 
+			getData = ' '+msg+' '; 
+			//myStatus = 1;         
+			});
+
+		server1.on("listening", function () {
+			myStatus = 2; });
+	    // listen on all IP adresses
+		server1.bind(listenerPort,listenerHOST);
+		connected = true; 	
+	} else {
+		alert ("Scratch already listening on udp ports"); 
+	}
+   };		    
+  
+   // end UDP Listener (experimental)
+   
+   // Cleanup function when the extension is unloaded
 
  
    // Block and block menu descriptions
